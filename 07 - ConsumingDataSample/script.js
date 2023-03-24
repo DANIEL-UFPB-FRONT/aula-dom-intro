213596b921f9a1ed15b7b4a1a56e02ee8856e80d
Daniel Faustino <daniel@dcx.ufpb.br>
2023-03-24 14:51:02 -0300

    Update consuming data sample

--- 07 - ConsumingDataSample/script.js
+++ 07 - ConsumingDataSample/script.js
@@ -1,14 +1,94 @@
-// https://servicodados.ibge.gov.br/api/v1/localidades/estados)
-// https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios)
+const state = {
+    pokemonRootContainerEnable: false,
+}
+const API_URL = 'https://pokeapi.co/api/v2/type/'
+
+const viewPokemonData = async (event) => {
+    const pokemon = document.getElementById('selectPokemonList').value
+    if (pokemon === 'default') {
+        (document.getElementById('predata') && document.getElementById('predata').remove())
+        return
+    }
+    
+    let data = document.getElementById('predata')
+    if (!data) {
+        data = document.createElement('pre')
+        data.id = 'predata'
+    }
+    
+    const pokemonDataRaw = await fetch(`${pokemon}`)
+    const pokemonData = await pokemonDataRaw.json()
+    data.innerHTML = `
+    Abilities: ${pokemonData.abilities.reduce((accumulator, element) => { return `${element.ability.name} ${accumulator}`},"")}
+    Name: ${pokemonData.name}
+    Height: ${pokemonData.height}
+    Weight: ${pokemonData.weight}
+    Species: ${pokemonData.species.name}
+    Base Experience: ${pokemonData.base_experience}
+    `
+    document.getElementById('pokemonRootContainer').append(data)
+}
 
 
-let pokemonData = null
-let pokemons = null
+const buildPokemonRootContainer = async (types) =>{
+
+    //Root container
+    const pokemonRootContainer = document.createElement('div')
+    pokemonRootContainer.setAttribute("id", "pokemonRootContainer")
+    const cabecalhoPokemon = document.createElement('h2')
+    cabecalhoPokemon.id = 'selectpokemon'
+    cabecalhoPokemon.textContent = 'Choose type'
+    pokemonRootContainer.append(cabecalhoPokemon)
+    document.body.append(pokemonRootContainer)
+
+
+    //Selection list
+    const selectPokemon = document.createElement('select');
+    selectPokemon.id = 'selectPokemonList'
+    pokemonRootContainer.append(selectPokemon)
+    const pokemonDefault = document.createElement('option')
+    pokemonDefault.id = 'pokemonDefault'
+    pokemonDefault.value = 'default'
+    pokemonDefault.innerText = 'Loading pokemons...'
+    selectPokemon.append(pokemonDefault)
+
+
+    //Populate list
+    pokemonData = await fetch(`${types}`)
+    pokemons = await pokemonData.json()
+    pokemons.pokemon.forEach(pokemonItem => {
+        const pokemonOption = document.createElement('option')
+        pokemonOption.innerText = pokemonItem.pokemon.name
+        pokemonOption.value = pokemonItem.pokemon.url
+        selectPokemon.append(pokemonOption)
+    });
+    pokemonDefault.innerText = 'Choose a pokemon...'
+
+
+    //Info buttom
+    const searchButtom = document.createElement('input')
+    searchButtom.type = 'submit'
+    searchButtom.value = 'See info'
+    searchButtom.id = 'seeInfo'
+    pokemonRootContainer.append(searchButtom)
+
+
+    document.getElementById('seeInfo').addEventListener('click', viewPokemonData)
+}
+
+const destroyPokemonRootContainer = () =>{
+    if(document.getElementById('pokemonRootContainer'))
+        document.getElementById('pokemonRootContainer').remove()
+
+    if(document.getElementById('seeInfo')){
+        document.getElementById('seeInfo').removeEventListener('click', viewPokemonData)
+    }
+}
+
 
 async function loadSpecies() {
-    const speciesData = await fetch('https://pokeapi.co/api/v2/type/')
+    const speciesData = await fetch(API_URL)
     const species = await speciesData.json()
-    console.log(species.results)
     const selectEstados = document.getElementById('select-species')
     species.results.forEach(item => {
         const speciesOption = document.createElement('option')
@@ -22,116 +102,17 @@ async function loadSpecies() {
     
 }
 
-
-async function selectPokemonEvent() {
-    const pokemonSelecionada = document.getElementById("selectPokemonList").value;
-    console.log('pokemon selecionada ', pokemonSelecionada)
-}
 async function addPokemonList(types) {
-
-    const cabecalhoPokemon = document.createElement('div')
-    cabecalhoPokemon.id = 'selectpokemon'
-    cabecalhoPokemon.innerHTML = '<h2>Choose type'
-    document.body.append(cabecalhoPokemon)
-
-    const pokemonContainer = document.createElement('div');
-    pokemonContainer.id = 'pokemonContainer';
-
-    const selectPokemon = document.createElement('select');
-    selectPokemon.id = 'selectPokemonList'
-
-    pokemonContainer.append(selectPokemon)
-
-    const pokemonDefault = document.createElement('option')
-    pokemonDefault.id = 'pokemonDefault'
-    pokemonDefault.value = 'default'
-    pokemonDefault.innerText = 'Loading pokemons'
-    selectPokemon.append(pokemonDefault)
-
-    document.body.append(pokemonContainer)
-    pokemonData = await fetch(`${types}`)
-    pokemons = await pokemonData.json()
-
-    pokemons.pokemon.forEach(pokemonItem => {
-        const pokemonOption = document.createElement('option')
-        pokemonOption.innerText = pokemonItem.pokemon.name
-        pokemonOption.value = pokemonItem.pokemon.url
-        selectPokemon.append(pokemonOption)
-    });
-    pokemonDefault.innerText = 'Choose a pokemon...'
-
-    document.getElementById('selectPokemonList').addEventListener('change', selectPokemonEvent)
-
-
-    const searchButtom = document.createElement('input')
-    searchButtom.type = 'submit'
-    searchButtom.value = 'See info'
-    searchButtom.id = 'seeInfo'
-    pokemonContainer.append(searchButtom)
-
-    document.getElementById('seeInfo').addEventListener('click', async () => {
-            const pokemon = document.getElementById('selectPokemonList').value
-
-            let data = document.getElementById('predata')
-            if (data === null) {
-                data = document.createElement('pre')
-                data.id = 'predata'
-            }
-
-            if (pokemon === 'default') {
-                data.innerHTML = ''
-                return
-            }
-
-            const pokemonDataRaw = await fetch(`${pokemon}`)
-            const pokemonData = await pokemonDataRaw.json()
-
-const test = pokemonData.abilities.reduce((accumulator, element) => {
-    console.log("accumulator")
-    console.log(accumulator)
-    console.log("element.ability.name")
-    console.log(element.ability.name)   
-
-    return `${accumulator}, ${element.ability.name}`
-
-},"")
-console.log(test)
-            data.innerHTML = `
-                Abilities: ${pokemonData.abilities.reduce((accumulator, element) => { return `${element.ability.name} ${accumulator}`},"")}
-                Name: ${pokemonData.name}
-                Height: ${pokemonData.height}
-                Weight: ${pokemonData.weight}
-                Species: ${pokemonData.species.name}
-                Base Experience: ${pokemonData.base_experience}
-                `
-            document.body.append(data)
-        }, )
+    state.pokemonRootContainerEnable ?
+        buildPokemonRootContainer(types) :
+        destroyPokemonRootContainer()
 }
 
-async function selectSpecies() {
-
-    
-
+async function selectType() {
     const selectedSpecies = document.getElementById("select-species").value;
-    let selectpokemonExist = !!document.getElementById("selectpokemon")
-    if(selectpokemonExist) {
-        document.getElementById('selectpokemon').removeEventListener('change', selectPokemonEvent)
-        document.getElementById("selectpokemon").remove()
-    }
-    selectpokemonExist = !!document.getElementById('selectPokemonList')
-    if(selectpokemonExist) document.getElementById("selectPokemonList").remove()
-
-    selectpokemonExist = !!document.getElementById('seeInfo')
-    if(selectpokemonExist) document.getElementById("seeInfo").remove()
-
-    selectpokemonExist = !!document.getElementById('predata')
-    if(selectpokemonExist) document.getElementById("predata").remove()
-    
-    if (selectedSpecies === 'default') return
-
+    state.pokemonRootContainerEnable = !(selectedSpecies === "default")
     addPokemonList(selectedSpecies)
-
 }
 
 window.addEventListener('load', loadSpecies)
-document.getElementById('select-species').addEventListener('change', selectSpecies)
+document.getElementById('select-species').addEventListener('change', selectType)
